6fb2f8a97605aefe83e79ca0a29fd30deebccfcf
voleti-sruthi <sruthi.lakshmi@m2pfintech.com>
2023-03-29 15:58:45 +0530

    Req1 : Add. Included Logic and Testcases for Calculating Area of A Rectangle

--- src/main/java/com/m2pfintech/GeometryRectangle.java
+++ src/main/java/com/m2pfintech/GeometryRectangle.java
@@ -1,4 +1,11 @@
 package com.m2pfintech;
 
+
 public class GeometryRectangle {
+    public double rectangleArea(double firstSide , double secondSide) throws IllegalArgumentException {
+        if(firstSide <=0 || secondSide <= 0){
+            throw new IllegalArgumentException();
+        }
+        return firstSide*secondSide;
+    }
 }
--- src/test/java/com/m2pfintech/GeometryRectangleTest.java
+++ src/test/java/com/m2pfintech/GeometryRectangleTest.java
@@ -1,5 +1,41 @@
 package com.m2pfintech;
 
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class GeometryRectangleTest {
     GeometryRectangle rectangleObject = new GeometryRectangle();
+    @Nested
+    class RectangleArea {
+        @Test
+        void toReturnAreaAsEightWhenSidesAreTwoAndFour() {
+            double expectedArea = 8;
+
+            double actualArea = rectangleObject.rectangleArea(2,4);
+
+            assertEquals(expectedArea,actualArea);
+        }
+
+        @Test
+        void toReturnAreaAsEightPointFourWhenSidesAreTwoPointOneAndFour()  {
+            double expectedArea = 8.4;
+
+            double actualArea = rectangleObject.rectangleArea(2.1,4);
+
+            assertEquals(expectedArea,actualArea);
+        }
+
+        @Test
+        void toThrowInvalidInputExceptionIfSidesAreNegativeValues() {
+
+            assertThrows(IllegalArgumentException.class,() -> rectangleObject.rectangleArea(-2,4));
+
+        }
+
+
+    }
+
 }
