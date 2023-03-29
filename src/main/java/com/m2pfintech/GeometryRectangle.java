f07019d1b56f33a33f6c7fbc48d475a7b8e9cd7f
voleti-sruthi <sruthi.lakshmi@m2pfintech.com>
2023-03-29 23:56:34 +0530

    req3 :Add. Included Logic,Testcases for Square Area and Perimeter

--- src/main/java/com/m2pfintech/GeometryRectangle.java
+++ src/main/java/com/m2pfintech/GeometryRectangle.java
@@ -2,17 +2,28 @@ package com.m2pfintech;
 
 
 public class GeometryRectangle {
-    public double rectangleArea(double firstSide , double secondSide) throws IllegalArgumentException {
+    private final double firstSide;
+    private final double secondSide;
+    public GeometryRectangle(double firstSide,double secondSide){
+        this.firstSide = firstSide;
+        this.secondSide = secondSide;
+    }
+    public static GeometryRectangle createSquare(double side){
+
+        return new GeometryRectangle(side,side);
+    }
+    public double rectangleArea() throws IllegalArgumentException {
         if(firstSide <=0 || secondSide <= 0){
             throw new IllegalArgumentException();
         }
         return firstSide*secondSide;
     }
 
-    public double rectanglePerimeter(double firstSide, double secondSide) {
+    public double rectanglePerimeter() {
         if(firstSide <=0 || secondSide <= 0){
             throw new IllegalArgumentException();
         }
         return 2*(firstSide+secondSide);
     }
+
 }
--- src/test/java/com/m2pfintech/GeometryRectangleTest.java
+++ src/test/java/com/m2pfintech/GeometryRectangleTest.java
@@ -3,64 +3,140 @@ package com.m2pfintech;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static com.m2pfintech.GeometryRectangle.createSquare;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class GeometryRectangleTest {
-    GeometryRectangle rectangleObject = new GeometryRectangle();
+
     @Nested
-    class RectangleArea {
-        @Test
-        void toReturnAreaAsEightWhenSidesAreTwoAndFour() {
-            double expectedArea = 8;
+    class Rectangle {
+        @Nested
+        class RectangleArea {
+            @Test
+            void toReturnAreaAsEightWhenSidesAreTwoAndFour() {
+                GeometryRectangle rectangleObject = new GeometryRectangle(2,4);
+                double expectedArea = 8;
 
-            double actualArea = rectangleObject.rectangleArea(2,4);
+                double actualArea = rectangleObject.rectangleArea();
 
-            assertEquals(expectedArea,actualArea);
+                assertEquals(expectedArea, actualArea);
+            }
+
+            @Test
+            void toReturnAreaAsEightPointFourWhenSidesAreTwoPointOneAndFour() {
+                GeometryRectangle rectangleObject = new GeometryRectangle(2.1,4);
+                double expectedArea = 8.4;
+
+                double actualArea = rectangleObject.rectangleArea();
+
+                assertEquals(expectedArea, actualArea);
+            }
+
+            @Test
+            void toThrowInvalidInputExceptionIfSidesAreNegativeValues() {
+                GeometryRectangle rectangleObject = new GeometryRectangle(-2,4);
+
+                assertThrows(IllegalArgumentException.class, rectangleObject::rectangleArea);
+
+            }
+
+
         }
 
-        @Test
-        void toReturnAreaAsEightPointFourWhenSidesAreTwoPointOneAndFour()  {
-            double expectedArea = 8.4;
+        @Nested
+        class RectanglePerimeter {
+            @Test
+            void toReturnPerimeterAsSixteenWhenSidesAreTwoAndSix() {
+                GeometryRectangle rectangleObject = new GeometryRectangle(2,6);
+                double expectedPerimeter = 16;
 
-            double actualArea = rectangleObject.rectangleArea(2.1,4);
+                double actualPerimeter = rectangleObject.rectanglePerimeter();
 
-            assertEquals(expectedArea,actualArea);
+                assertEquals(expectedPerimeter, actualPerimeter);
+            }
+
+            @Test
+            void toReturnPerimeterAsSixteenPointFourWhenSidesAreTwoPointTwoAndSix() {
+                GeometryRectangle rectangleObject = new GeometryRectangle(2.2,6);
+                double expectedPerimeter = 16.4;
+
+                double actualPerimeter = rectangleObject.rectanglePerimeter();
+
+                assertEquals(expectedPerimeter, actualPerimeter);
+            }
+
+            @Test
+            void toThrowInvalidInputExceptionIfSidesAreNegativeValues() {
+                GeometryRectangle rectangleObject = new GeometryRectangle(-2,4);
+                assertThrows(IllegalArgumentException.class, rectangleObject::rectanglePerimeter);
+            }
         }
-
-        @Test
-        void toThrowInvalidInputExceptionIfSidesAreNegativeValues() {
-
-            assertThrows(IllegalArgumentException.class,() -> rectangleObject.rectangleArea(-2,4));
-
-        }
-
-
     }
 
     @Nested
-    class RectanglePerimeter{
-        @Test
-        void toReturnPerimeterAsSixteenWhenSidesAreTwoAndSix(){
-            double expectedPerimeter = 16;
+    class Square {
+        @Nested
+        class SquareArea {
+            @Test
+            void toReturnAreaAsTwentyFiveWhenSideOfSquareIsFive() {
+                GeometryRectangle squareObject = createSquare(5);
+                double expectedArea = 25;
 
-            double actualPerimeter = rectangleObject.rectanglePerimeter(2,6);
+                double actualArea = squareObject.rectangleArea();
 
-            assertEquals(expectedPerimeter,actualPerimeter);
+
+                assertEquals(expectedArea, actualArea);
+            }
+            @Test
+            void toReturnAreaAsFourPointFourOneWhenSideOfSquareIsTwoPointOne() {
+                GeometryRectangle squareObject = createSquare(2.1);
+                double expectedArea = 4.41;
+
+                double actualArea = squareObject.rectangleArea();
+
+                assertEquals(expectedArea, actualArea);
+            }
+
+
+            @Test
+            void toThrowInvalidInputExceptionIfSidesAreNegativeValues() {
+                GeometryRectangle squareObject = createSquare(-5);
+
+                assertThrows(IllegalArgumentException.class, squareObject::rectangleArea);
+
+            }
+
+
         }
 
-        @Test
-        void toReturnPerimeterAsSixteenPointFourWhenSidesAreTwoPointTwoAndSix(){
-            double expectedPerimeter = 16.4;
+        @Nested
+        class SquarePerimeter {
+            @Test
+            void toReturnPerimeterAsSixteenWhenSideOfSquareIsFour() {
+                GeometryRectangle squareObject = createSquare(4);
+                double expectedPerimeter = 16;
 
-            double actualPerimeter = rectangleObject.rectanglePerimeter(2.2,6);
+                double actualPerimeter = squareObject.rectanglePerimeter();
 
-            assertEquals(expectedPerimeter,actualPerimeter);
-        }
+                assertEquals(expectedPerimeter, actualPerimeter);
+            }
 
-        @Test
-        void toThrowInvalidInputExceptionIfSidesAreNegativeValues() {
-            assertThrows(IllegalArgumentException.class, () -> rectangleObject.rectanglePerimeter(-2, 4));
+            @Test
+            void toReturnPerimeterAsSixteenPointFourWhenSideOfSquareIsFourPointOne() {
+                GeometryRectangle squareObject = createSquare(4.1);
+                double expectedPerimeter = 16.4;
+
+                double actualPerimeter = squareObject.rectanglePerimeter();
+
+                assertEquals(expectedPerimeter, actualPerimeter);
+            }
+
+            @Test
+            void toThrowInvalidInputExceptionIfSideIsNegativeValues() {
+                GeometryRectangle squareObject = createSquare(-2);
+                assertThrows(IllegalArgumentException.class, squareObject::rectanglePerimeter);
+            }
         }
     }
 
